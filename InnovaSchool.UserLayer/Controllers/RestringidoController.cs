1e832be33abb7fab0ea2a1621e1dfcd0e1001dfe
gespilco <gespilco@gmail.com>
2016-09-06 20:05:24 -0500

    restr

---
+++ InnovaSchool.UserLayer/Controllers/RestringidoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InnovaSchool.UserLayer.Controllers
+{
+    public class RestringidoController : Controller
+    {
+        //
+        // GET: /Restringido/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
